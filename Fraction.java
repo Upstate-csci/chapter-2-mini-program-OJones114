7461f3605a6b69b0bd28f05468bba4491344ed52
OJones114 <97766608+OJones114@users.noreply.github.com>
2022-02-05 18:57:18 -0500

    Create Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -5,15 +5,23 @@ public class Fraction
 	public static void main(String[] args)
 	{
     // declare a scanner object for input
-		Scanner scan = new 
+		Scanner scan = new Scanner(System.in);
 
     // prompt for numerator and read in - use print instead of println so the user will input on the same line as the prompt
+		System.out.print("Numerator: ");
+		int num1 = scan.nextInt();
     
     // prompt for denomator and read in - use print instead of println so the user will input on the same line as the prompt
+		System.out.print("Denomator: ");
+		int den1 = scan.nextInt();
     
     // compute the answer and store in a variable of type double
+		double answer;
+		answer = (num1 / den1);
     
     // ouutput the answer
+		System.out.println(answer);
+	
     
   }
 }
